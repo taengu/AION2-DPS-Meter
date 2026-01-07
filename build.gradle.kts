151157893a1fe460fe123151e54d9b7c5bc9a471
TK-open-public <itaebread2@gmail.com>
2026-01-07 19:20:56 +0900

    Feat: gradle Groovy > kotlin 변경

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    kotlin("jvm")
+    id("org.jetbrains.compose")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+group = "com.tbread"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+}
+
+dependencies {
+    // Note, if you develop a library, you should use compose.desktop.common.
+    // compose.desktop.currentOs should be used in launcher-sourceSet
+    // (in a separate module for demo project and in testMain).
+    // With compose.desktop.common you will also lose @Preview functionality
+    implementation(compose.desktop.currentOs)
+    implementation ("org.pcap4j:pcap4j-core:1.8.2")
+    implementation ("org.pcap4j:pcap4j-packetfactory-static:1.8.2")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "aion2meter4j"
+            packageVersion = "1.0.0"
+        }
+    }
+}
