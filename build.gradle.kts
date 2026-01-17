7c6af6453d28d755bcbe02ada85857f98c5a5197
TK-open-public <itaebread2@gmail.com>
2026-01-17 19:37:59 +0900

    디스크립션 및 vendor 삭제

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,9 +59,7 @@ compose.desktop {
             targetFormats(TargetFormat.Msi)
             packageName = "aion2meter4j"
             packageVersion = "1.0.0"
-            description = "아이온2 전투분석을 위한 미터기"
-            copyright = "© 2026 TK-open-public. Licensed under MIT License"
-            vendor = "TeamSupSin"
+            copyright = "Copyright 2026 TK open public Licensed under MIT License"
         }
 
 
