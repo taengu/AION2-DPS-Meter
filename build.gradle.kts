c1c13ea06081b0ef84630d6ac41942aa834d2e85
Max Watson <maxw247@gmail.com>
2026-02-07 12:41:36 +0800

    Configure native-image deadlock watchdog

--- build.gradle.kts
+++ build.gradle.kts
@@ -69,6 +69,8 @@ graalvmNative {
             buildArgs.add("-Dprism.fontdir=C:\\Windows\\Fonts")
             buildArgs.add("--no-fallback")
             buildArgs.add("--enable-native-access=javafx.base,javafx.graphics,javafx.controls,javafx.web,javafx.media")
+            buildArgs.add("-H:DeadlockWatchdogInterval=10")
+            buildArgs.add("-H:+DeadlockWatchdogExitOnTimeout")
 
             // Critical for UI and async behavior
             buildArgs.add("--initialize-at-build-time=javafx,com.sun.javafx,kotlinx.coroutines")
