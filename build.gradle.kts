76be99346f98f4c124f5e4c8cdd810cca63104b9
Max Watson <maxw247@gmail.com>
2026-02-07 12:41:30 +0800

    Enable native access for JavaFX modules

--- build.gradle.kts
+++ build.gradle.kts
@@ -68,6 +68,7 @@ graalvmNative {
             buildArgs.add("-H:+AddAllCharsets")
             buildArgs.add("-Dprism.fontdir=C:\\Windows\\Fonts")
             buildArgs.add("--no-fallback")
+            buildArgs.add("--enable-native-access=javafx.base,javafx.graphics,javafx.controls,javafx.web,javafx.media")
 
             // Critical for UI and async behavior
             buildArgs.add("--initialize-at-build-time=javafx,com.sun.javafx,kotlinx.coroutines")
