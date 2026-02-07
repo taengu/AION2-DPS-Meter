d6390ee27c9c8e73ef91ae82873395a0e84d8eec
Max Watson <maxw247@gmail.com>
2026-02-07 20:48:43 +0800

    Add localized identifying target label

--- src/main/resources/js/core.js
+++ src/main/resources/js/core.js
@@ -2214,7 +2214,7 @@ class DpsApp {
       return this.getDefaultTargetLabel(targetMode);
     }
     if (targetMode === "lastHitByMe" && (!Number(targetId) || Number(targetId) <= 0) && !targetName) {
-      return "Identifying you...";
+      return this.i18n?.t("target.identifying", "Identifying you...") ?? "Identifying you...";
     }
     if (Number.isFinite(Number(targetId)) && Number(targetId) > 0) {
       return `Mob #${Number(targetId)}`;
